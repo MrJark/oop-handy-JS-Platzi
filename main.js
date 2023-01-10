de39c7869ccec1a8adb147cf3496d12ada949f8d
MrJark <mrjark7@gmail.com>
2023-01-10 14:54:06 +0100

    've added prototypes

--- main.js
+++ main.js
@@ -13,3 +13,25 @@ const chema = {
 
 //creación de un prototype
 
+function Student(name, age, cursosAprobados) {
+    this.name = name;
+    this.age = age; 
+    this.cursosAprobados = cursosAprobados;
+    //this.aprobarCursos = function (nuevoCurso) { //esto nos crea un método dentro del prototype pero vamos a trabajar ahora de otra manera
+    //    this.cursosAprobados.push(nuevoCurso);
+    //}
+}
+
+//con esto estamos creando un método fuera de la función Student pero al poner la herramienta .prototype me lo acepta
+Student.prototype.aprobarCursos = function (nuevoCurso) { 
+    this.cursosAprobados.push(nuevoCurso);
+}
+
+const juanita = new Student ( //esto es un prototype porque esto se crean con la palabra reservada ' new '
+    "Juanita Margarita",
+    17,
+    [
+        "Curso de Growth Marketing",
+        "Curso de Responsve Design",
+    ],
+);//tiene 3 parámetros porque así lo hemos decidido cen la función Student, le damos estos 3 parámetros
